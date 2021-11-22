c663ee9255a347fd42168af404d3b4bad7857629
nangchun84 <94749920+nangchun84@users.noreply.github.com>
2021-11-23 00:49:34 +0900

    Add files via upload

---
+++ upbit_API.py
@@ -0,0 +1,25 @@
+import pyupbit
+import time
+import datetime
+
+def get_balances(upbit, ticker, value):
+    # 종목별 세부항목 조회
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b[value] is not None:
+                return float(b[value])
+            else:
+                return 0
+    return 0
+
+
+def get_current_price(ticker):
+    # 현재가 조회
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+def get_ma15(ticker):
+    # 15일 이동 평균선 조회
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
